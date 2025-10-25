284a415277980062fc41747430b431d31e92f364
Pr0meTheuSS <66156953+Pr0meTheuSS@users.noreply.github.com>
2025-10-25 08:43:29 +0700

    Feature/init project (#1)
    * Add build and .vscode in .gitignore
    
    * Set up .gitignore, add empty READMY, add base MIT License
    
    * Init raylib hello-world project with CMake, conan
    
    * Up cmake version, remove unused dependencies

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "raylib.h"
+
+int main() {
+    InitWindow(800, 450, "Hello Raylib!");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawText("Hello, world!", 350, 200, 20, DARKBLUE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
